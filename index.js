98857d8b8f259d54fc60987cb150fc31d267d8d6
ANKIT161020 <116353008+ANKIT161020@users.noreply.github.com>
2023-11-12 00:37:13 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer.', 'Web Designer.','Gamer.'],
+    typeSpeed: 50,
+    loop:true,
+    loopCount:Infinity,
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 500,
+  });
